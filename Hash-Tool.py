157e52c55e4a309855824559967b6e7ac8160128
Castro-Erick <150728304+Castro-Erick@users.noreply.github.com>
2023-12-07 13:29:58 -0800

    Changed Hash Output
    Removed the functionality of specifying a Hashing algorithm and instead outputs hashes using the most common algorithms.

--- Hash-Tool.py
+++ Hash-Tool.py
@@ -1,26 +1,24 @@
 import subprocess
 
-PS_Command = 'Get-FileHash'
+PS_COMMAND = 'Get-FileHash'
+HASH_ALGORITHMS = ["MD4", "MD5", "SHA1", "SHA256", "SHA512"]
 
 print("Type \"quit\" at any point to exit the program.")
 
 while True:
-    hash_func = input("Enter the Hash Function you would like to use: ")
-    if hash_func.lower() == "quit":
-        break
-
-    file_path = input("Enter an existing file path: ")
+    file_path = input("\nEnter an existing file path: ")
     if file_path.lower() == "quit":
         break
 
-    command_string = ['certutil', '-hashfile', file_path, hash_func.upper()]
-
     try:
-        Result = subprocess.run(command_string, check=True, capture_output=True, text=True)
-    
-        output = Result.stdout
+        for algorithm in HASH_ALGORITHMS:
+            command_string = ['certutil', '-hashfile', file_path, algorithm]
+            Result = subprocess.run(command_string, check=True, capture_output=True, text=True)
 
-        print(output)
+            output = Result.stdout.splitlines()
+            hash_val = output[1].strip() if len(output) > 1 else "N/A"
+
+            print(f'\n{algorithm}: {hash_val}')
 
     except subprocess.CalledProcessError as e:
         print(f'Error: {e}')
